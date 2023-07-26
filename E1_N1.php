b38184b04244ef0ef5fa7dd52d254e785e5670cd
Patricia Manzanas <pmanzanastomas@gmail.com>
2023-07-26 09:28:35 +0200

    Exercici 1 N1

--- E1_N1.php
+++ E1_N1.php
@@ -1,3 +1,49 @@
 <?php
 
+abstract class Animal{ 
+    public $nom;
+
+    public function __construct($nom){
+        $this->nom=$nom;
+    }
+
+    abstract public function makeSound();
+
+    public function mensaje(){
+        return $this->nom." dice: ";
+    }
+}
+
+class Gat extends Animal{
+
+    public function __construct($nom){
+        $this->nom=$nom;
+    }
+
+    function makeSound(){
+        return "meaaaawww!!!";
+    }
+}
+
+class Gos extends Animal{
+
+    public function __construct($nom){
+        $this->nom=$nom;
+    }
+
+    function makeSound(){
+        return "Guau guau!!!";
+    }
+}
+
+$gat=new Gat("Misino");
+echo $gat->mensaje().$gat->makeSound();
+
+echo "<br>";
+
+$gos=new Gos ("Perrete");
+echo $gos->mensaje().$gos->makeSound();
+
+
+
 ?>
